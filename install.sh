eaa6890105a9f24255b881e5ff6e2222d1411347
ShadowNetter <shadownetter@gmail.com>
2025-09-25 21:32:01 +0200

    added install file

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo
+echo "crawl"
+echo "by ShadowNetter"
+echo
+echo "cloning into repo..."
+git clone https://github.com/ShadowNetter-Official/crawl
+cd crawl
+echo "done"
+echo "installing..."
+cargo build --release
+cp target/release/crawl ~/.cargo/bin/
+echo "done"
+echo
+echo "to uninstall do: "
+echo "rm ~/.cargo/bin/crawl"
+echo
